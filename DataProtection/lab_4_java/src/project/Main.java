479bb595efbb28fafdd2c3e3ed6995d68ffc006d
mifeus42 <maksim.romashkin.2@gmail.com>
2022-03-05 17:48:12 +0300

    do labs

--- DataProtection/lab_4_java/src/project/Main.java
+++ DataProtection/lab_4_java/src/project/Main.java
@@ -1,8 +1,18 @@
 package project;
 
+
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
     public static void main(String[] args) {
+        String key = "test Key";
+        String content = "test1234 test5678 9876test";
+        BlowFish blowFish = new BlowFish(key);
 
+        byte[] tmpEncode = blowFish.encode(content.getBytes(StandardCharsets.UTF_8));
+        byte[] tmpDecode = blowFish.decode(tmpEncode);
+
+        System.out.println(new String(tmpDecode).replaceFirst("^\0*", ""));
     }
 }
